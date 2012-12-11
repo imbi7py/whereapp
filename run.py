55f6e9b436c0d6be92e9e31da7d44cebc7319f9f
J. Randall Hunt <randallhunt@gmail.com>
2012-12-11 03:12:12 -0500

    Basic working page

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import sys
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main_page():
+    info = {}
+    return render_template('index.html', info=info)
 
 if __name__ == '__main__':
     sys.exit(app.run(host='0.0.0.0'))
