baf882405801d15a7be9af653b8b832fa84c0708
Randall Hunt <randallhunt@gmail.com>
2013-02-19 16:36:51 +0000

    fixed a bunch of stuff

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, Response, request, redirect
 
 import foursquare
 import yaml
+import logging
+logging.basicConfig()
+logger = logging.getLogger('foursquare')
 
 _config = yaml.safe_load(file('config.yaml'))
 
@@ -9,17 +12,16 @@ _config = yaml.safe_load(file('config.yaml'))
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+redirect_uri = _config['flask']['base_url'] + _config['foursquare']['redirect_uri']
 foursqclient = foursquare.Foursquare(
                 client_id=_config['foursquare']['client_id'],
                 client_secret=_config['foursquare']['client_secret'],
-                redirect_uri=_config['flask']['base_url'] +
-                             _config['foursquare']['redirect_uri'])
+                redirect_uri=redirect_uri)
 
 
 @app.route(_config['foursquare']['redirect_uri'])
 def foursquare_oauth():
     access_token = foursqclient.oauth.get_token(request.args.get('code'))
-    print access_token
     # db store foursq access token
     foursqclient.set_access_token(access_token)
     return redirect(_config['flask']['base_url'], 302)
@@ -27,7 +29,8 @@ def foursquare_oauth():
 
 @app.route('/foursquare/auth')
 def foursquare_auth():
-    return redirect(foursqclient.oauth.auth_url(), 302)
+    auth_uri = foursqclient.oauth.auth_url()
+    return redirect(auth_uri, 302)
 
 
 @app.route('/')
@@ -38,4 +41,4 @@ def main_page():
 
 if __name__ == '__main__':
     app.debug = _config['flask']['debug']
-    app.run()
+    app.run(host=_config['flask']['host'], port=_config['flask']['port'])
